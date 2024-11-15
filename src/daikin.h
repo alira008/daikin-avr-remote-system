84a98d5777f63f0e03f2893853546d56eb0670f2
ariel <ariellira.al@gmail.com>
2024-11-15 11:56:29 -0800

    added for daikin reqs

---
+++ src/daikin.h
@@ -0,0 +1,9 @@
+#ifndef __DAIKIN_H__
+#define __DAIKIN_H__
+
+// daikin requirements
+#define HIGH_STATE_DURATION 450 // 450us
+#define LOW_ONE_STATE_DURATION 1286 // 1286us
+#define LOW_ZERO_STATE_DURATION 420 // 420us
+
+#endif // !__DAIKIN_H__
