23cc98c37f455a6e5a74a211e27b8cec2d247878
ariel <ariellira.al@gmail.com>
2024-11-15 13:04:07 -0800

    removed unnecessary code

--- src/main.c
+++ src/main.c
@@ -2,13 +2,9 @@
 #include <avr/io.h>
 
 int main(void) {
-  timer1_init();
+  timer3_init();
 
-  PORTB |= (1 << PB1);
   while (1) {
-    while (!g_timer1_flag)
-      ;
-    g_timer1_flag = 0;
   }
   return 1;
 }
--- src/timer.h
+++ src/timer.h
@@ -5,35 +5,12 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 
-volatile uint8_t g_timer1_flag = 0;
-
-static inline void timer1_init() {
-  // settings clear timer on compare (CTC)
-  // using prescaler 64
-  // this means TCNT1 will count to 125,000 ticks/s
-  TCCR1B = _BV(WGM13) | _BV(CS11) | _BV(CS10);
-
-  // compare TCNT1 with this value
-  OCR1A = 125;
-
-  // reseting TCNT1
-  TCNT1 = 0;
-
-  // enable interrupts for Output Compare A
-  TIMSK1 = _BV(OCIE1A);
-
-  // enable interrupts globally
-  SREG |= _BV(SREG_I);
-}
-
-ISR(TIMER1_COMPA_vect) { g_timer1_flag = 1; }
-
 static inline void timer3_init() {
   // normal mode
   TCCR3A = 0;
   // enable ctc mode
   // 8 prescaler
-  // this sets our 1us per tick
+  // this sets our 1us tick 
   TCCR3B = _BV(WGM32) | _BV(CS31);
 
   // start on a high state
